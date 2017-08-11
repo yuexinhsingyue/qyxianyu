9033b727ab719b164278ea98f0b8a2cbc3694009
yuexinhsingyue <412140451@qq.com>
2017-08-11 09:15:45 +0800

    商品分类

--- resources/views/admin/datastati/salestat.blade.php
+++ resources/views/admin/datastati/salestat.blade.php
@@ -21,20 +21,30 @@
                         </progress> 
                         </div>
                     <div class="col-xs-12 col-sm-3 stat-col">
-                        <div class="stat-icon"> <i class="fa fa-shopping-cart"></i> </div>
+                        <div class="stat-icon"> <i class="fa fa-suitcase"></i> </div>
                         <div class="stat">
-                            <div class="value"> 78464 </div>
+                            <div class="value"> {{$totelOrder}} </div>
                             <div class="name"> 总订单数</div>
                         </div> <progress class="progress stat-progress" value="25" max="100">
                             <div class="progress">
                                 <span class="progress-bar" style="width: 25%;"></span>
                             </div>
                         </progress> </div>
+                   <div class="col-xs-12 col-sm-3 stat-col">
+                    <div class="stat-icon"> <i class="fa fa-suitcase"></i> </div>
+                    <div class="stat">
+                        <div class="value"> {{$totelgoods}} </div>
+                        <div class="name"> 累计交易商品数量</div>
+                    </div> <progress class="progress stat-progress" value="25" max="100">
+                        <div class="progress">
+                            <span class="progress-bar" style="width: 25%;"></span>
+                        </div>
+                    </progress> </div>
                     <div class="col-xs-12 col-sm-3  stat-col">
                         <div class="stat-icon"> <i class="fa fa-line-chart"></i> </div>
                         <div class="stat">
-                            <div class="value"> $80.560 </div>
-                            <div class="name"> 当月交易额 </div>
+                            <div class="value"> ${{$priceMonth}}</div>
+                            <div class="name"> 总交易金额</div>
                         </div> <progress class="progress stat-progress" value="60" max="100">
                             <div class="progress">
                                 <span class="progress-bar" style="width: 60%;"></span>
@@ -43,7 +53,7 @@
                     <div class="col-xs-12 col-sm-3  stat-col">
                         <div class="stat-icon"> <i class="fa fa-users"></i> </div>
                         <div class="stat">
-                            <div class="value"> 359 </div>
+                            <div class="value"> {{$userids}} </div>
                             <div class="name"> 总用户 </div>
                         </div> <progress class="progress stat-progress" value="34" max="100">
                             <div class="progress">
@@ -51,20 +61,21 @@
                             </div>
                         </progress> </div>
                     <div class="col-xs-12 col-sm-3  stat-col">
-                        <div class="stat-icon"> <i class="fa fa-list-alt"></i> </div>
+                        <div class="stat-icon"> <i class="fa fa-shopping-cart"></i> </div>
                         <div class="stat">
-                            <div class="value"> 59 </div>
+                            <div class="value"> {{$orderMonth}} </div>
                             <div class="name"> 当月订单数 </div>
                         </div> <progress class="progress stat-progress" value="49" max="100">
                             <div class="progress">
                                 <span class="progress-bar" style="width: 49%;"></span>
                             </div>
                         </progress> </div>
+
                     <div class="col-xs-12 col-sm-3 stat-col">
                         <div class="stat-icon"> <i class="fa fa-dollar"></i> </div>
                         <div class="stat">
-                            <div class="value"> $780.064 </div>
-                            <div class="name"> 总交易金额 </div>
+                            <div class="value"> ${{$priceMonth}} </div>
+                            <div class="name">  当月交易额</div>
                         </div> <progress class="progress stat-progress" value="15" max="100">
                             <div class="progress">
                                 <span class="progress-bar" style="width: 15%;"></span>
@@ -78,18 +89,28 @@
     <div class="col">  
     <div class="card  stats">  
         <div id="main" style="width: 900px;height:400px;"></div>
-        <!-- 接收控制器传递的表格数据，中转给js接收 -->
-        <input type="hidden" name='chartX' value = "{{$chartX}}">
+        <?php
+            $array = $countOrder;
+            // var_dump($array);
+        ?>
         <script type="text/javascript" src="/admin/js/jquery.js"></script>
         <script type="text/javascript" src="/admin/js/echarts.js"></script>
         <script type="text/javascript">
             // 基于准备好的dom，初始化echarts实例
             var myChart = echarts.init(document.getElementById('main'));
-            // var echartX = ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子",'sdfsd'];
-            var echartX = $("input[name='chartX']").val();
-            var echartX=echartX.split(","); 
 
-            var echartY = [50, 20, 36, 10, 10, 20,50,25,65,50, 20, 36, 10, 10, 20,50,25,65,65,50, 20, 36, 10, 10, 20,50,25,65,25,65];
+            var readpoint = <?php echo json_encode($array);?>;  
+
+            var echartX = [];   //X轴数据
+            var echartY = [];   //Y轴数据
+            for(var tmp in readpoint){
+
+                echartX.push(tmp);  //将键名压入X轴  日期
+                echartY.push(readpoint[tmp]);   //将值压入Y轴 金额
+
+            }
+
+            // var echartY = [50, 20, 36, 10, 10, 20,50,25,65,50, 20, 36, 10, 10, 20,50,25,65,65,50, 20, 36, 10, 10, 20,50,25,65,25,65];
             // 指定图表的配置项和数据
             var option = {
                 title: {
