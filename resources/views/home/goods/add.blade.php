44b97638bdc7344235f3f72a83d85d44f322cd9e
SSunyshine <1229460719@qq.com>
2017-08-14 21:31:52 +0800

    个人中心页面搭建完成

---
+++ resources/views/home/goods/add.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.person')
+@section('title','商品添加页')
+
+@section('content1')
+
+    商品添加
+    @endsection
---
+++ resources/views/home/goods/list.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.person')
+@section('title','商品列表页')
+
+@section('content1')
+
+   商品列表页
+@endsection
