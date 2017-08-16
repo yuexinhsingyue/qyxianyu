9c270c6597224db36503d817ad8db3d80d325b29
SSunyshine <1229460719@qq.com>
2017-08-16 23:45:27 +0800

    前台商品模块基础完成

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Model\Advert;
+use App\Http\Model\Car;
 use App\Http\Model\Goods;
 use App\Http\Model\Type;
 use Illuminate\Http\Request;
@@ -20,15 +21,8 @@ class IndexController extends Controller
     {
 
         //获取首页上所有电脑分类的信息
-<<<<<<< HEAD
-        $com = Goods::where('tid',30)->get();
-=======
-
-//        $com = Type::where('tid',38)->get();
 
         $com = Goods::where('tid',30)->get();
-
->>>>>>> 250b8d83d6d1284c6abbb6353d5e69378f28747c
         //获取商品分类里的所有父类
         $ptype = Type::where('pid',0)->get();
         foreach($ptype as $k => $v){
@@ -36,21 +30,15 @@ class IndexController extends Controller
             //遍历商品表父级下的二级分类
          $a[] = Type::where('pid',$v->tid)->get();
         }
-
+        //购物车中里的信息
+        $count =  count(Car::get());
         //  广告位
         $advert = Advert::where('status',1)->groupBy('adposition')->orderBy('adposition')-> get();
-<<<<<<< HEAD
-        // 轮播图
-        $figure = Slid::where('status','=',1)->get();
-        return view('home.index',compact('ptype','a','advert','figure','com'));
-=======
-
 
         // 轮播图
         $figure = Slid::where('status','=',1)->get();
+        return view('home.index',compact('ptype','a','advert','figure','com','count'));
 
-        return view('home.index',compact('ptype','a','advert','com','figure'));
->>>>>>> 250b8d83d6d1284c6abbb6353d5e69378f28747c
     }
     //前台大厅列表页
     public function list()
@@ -63,18 +51,23 @@ class IndexController extends Controller
             $a[] = Type::where('pid',$v->tid)->get();
         }
         $goods = Goods::all();
+        //购物车中里的信息
+        $count =  count(Car::get());
        // $goods = $goods -> where('gname','like','%'.Input::get('search').'%') -> paginate(2);
-        return view('home.list',compact('ptype','a','goods'));
+        return view('home.list',compact('ptype','a','goods','count'));
     }
     //详情页
     public function detail($id)
     {
         //获取商品表的信息
         $input = Goods::find($id);
+        //购物车中里的信息
+        $count =  count(Car::get());
         //获取此商品的信息
 //        dd($input);
-        return view('home.detail',compact('input','id'));
+        return view('home.detail',compact('input','id','count'));
     }
+
 
     //个人中心页
     public function person()
@@ -96,16 +89,56 @@ class IndexController extends Controller
     //购物车
     public function car($id)
     {
-        //获取商品表的信息
         $input = Goods::find($id);
+        //获取gid加入到购物车中
+        $car = new Car();
+        $car->gid = $id;
+        $car->save();
+        //获取商品表的信息
+        $goods = Goods::get();
+        foreach($goods as $k=>$v){
+            if($v['id']== $car['gid']){
+                $a = [];
+            }
+            $a = $goods;
+        }
+        //购物车中里的信息
+        $count =  count(Car::get());
         //获取购买东西的总价格
         $price = $input['nprice']*$input['goodsNum'];
-        return view('home.car',compact('input','id','price'));
+        return view('home.car',compact('input','id','price','car','a','count'));
+    }
+    //删除购物车的商品
+    public function delCar($id)
+    {
+        //删除商品
+        $input = Goods::find($id);
+        $car = Car::where('gid',$id)->delete();
+        return view('home.car',compact('id','car'));
     }
     //订单页
-    public function pay()
+    public function pay($id)
     {
-        return view('home.pay');
+        $input = Goods::find($id);
+        //获取gid加入到购物车中
+        $car = new Car();
+        $car->gid = $id;
+        $car->save();
+        //获取商品表的信息
+        $goods = Goods::get();
+        foreach($goods as $k=>$v){
+            if($v['id']== $car['gid']){
+                $a = [];
+            }
+            $a = $goods;
+        }
+        //购物车中里的信息
+        $count =  count(Car::get());
+        //获取购买东西的总价格
+        $price = $input['nprice']*$input['goodsNum'];
+        //购物车中里的信息
+        $count =  count(Car::get());
+        return view('home.pay',compact('input','id','price','car','a','count'));
     }
     //订单完成页
     public function success()
